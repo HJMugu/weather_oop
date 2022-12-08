63c61211f16166cddf7947a8e07fb053cb97dffa
Henri <henri-joel.mugu@voco.ee>
2022-12-08 07:19:07 +0200

    Change weather data from API

--- classes/Weather.js
+++ classes/Weather.js
@@ -1,5 +1,5 @@
 class Weather{
-    constructor( city ) {
+    constructor(city) {
         this.city = city
         this.key = 'a23e25bc421ba3eb2540db15a33dd6a7'
     }
@@ -8,5 +8,9 @@ class Weather{
         const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?q=${this.city}&appid=${this.key}`)
         const responseData = await response.json()
         return responseData
+    }
+
+    changeCity(city){
+        this.city = city
     }
 }
