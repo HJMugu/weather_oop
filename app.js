627f1b49b7ec5639542fce7d3da62701ae3c679d
Henri <henri-joel.mugu@voco.ee>
2022-12-08 07:33:00 +0200

    Set city to LS and change weather data from LocalStorage

--- app.js
+++ app.js
@@ -1,7 +1,13 @@
+//LS objekt
+
+const ls = new LS()
+//init city data
+const initCity = ls.getCity()
 // Weather object
-const weather = new Weather('Tallinn')
+const weather = new Weather(initCity)
 // UI object
 const ui = new UI()
+
 
 //events
 const form = document.querySelector("#change-city")
@@ -12,6 +18,7 @@ function changeWeather(event){
 
     const city = document.querySelector('#city-name').value
     weather.changeCity(city)
+    ls.setCity(city)
     getWeather()
     document.querySelector('#city-name').value = ''
     event.preventDefault()
